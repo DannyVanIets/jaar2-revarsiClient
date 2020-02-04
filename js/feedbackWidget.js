deec19d96a047e87a490757d22f78cd8e6874925
DannyVanIets <dannyvanbokhorst@live.nl>
2020-02-04 20:44:02 +0100

    Chipmunk: level 1 t/m opdracht 5

---
+++ js/feedbackWidget.js
@@ -0,0 +1,25 @@
+$(function () {
+    console.log("Ready!");
+    let fw = new FeedbackWidget("feedback-danger");
+    fw.show();
+});
+
+class FeedbackWidget {
+    constructor(elementId) {
+        this._elementId = elementId;
+    }
+
+    get elementId() { //getter, set keyword voor setter methode
+        return this._elementId();
+    }
+
+    show() {
+        var x = document.getElementById(this._elementId);
+        x.style.display = "block";
+    }
+
+    hide() {
+        var x = document.getElementById(this._elementId);
+        x.style.display = "none";
+    }
+}
