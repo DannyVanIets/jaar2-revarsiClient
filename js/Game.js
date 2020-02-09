4855953e790128a030a38c2efd689d12d7347bbf
DannyVanIets <dannyvanbokhorst@live.nl>
2020-02-09 15:47:24 +0100

    Chipmunk: level 2 taak 2

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,15 @@
 const Game = (function(){
-    console.log('hallo, vanuit een module')
+    console.log('hallo, vanuit een module');
+
+    //Private function init
+    const privateInit = function(){
+        console.log("Private information!");
+    }
+
+    //Waarde/object geretourneerd aan de outer scope
+    return {
+        init: privateInit
+    }
 })();
+
+Game.init();
