bfec44085e8cb4f3318b6f0656d527f816a8a9bc
DannyVanIets <dannyvanbokhorst@live.nl>
2020-02-23 19:59:10 +0100

    Code sapien level 5 taak 4

--- sapien/gulpfile.js/tasks/js.js
+++ sapien/gulpfile.js/tasks/js.js
@@ -1,7 +1,12 @@
+const {src, dest} = require('gulp');
+
 const fn = function(voornaam) {
     return function () {
-        console.log("Taak js is uitgevoerd, " + voornaam + "!");
-        return Promise.resolve("Klaar");
+        //console.log("Taak js is uitgevoerd, " + voornaam + "!");
+        //return Promise.resolve("Klaar");
+
+        return src("js/*.js")
+            .pipe(dest("dist"));
     }
 };
 
