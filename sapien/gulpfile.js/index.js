6d5193316b3708d5e92cf48830a53696116c2a7b
Danny van Bokhorst <dannyvanbokhorst@live.nl>
2020-03-01 22:05:48 +0100

    Toegevoegd dat je de bowser sync kan doen

--- sapien/gulpfile.js/index.js
+++ sapien/gulpfile.js/index.js
@@ -1,4 +1,6 @@
 const config = require('./config');
+const {series, parallel, watch} = require('gulp');
+var browserSync = require('browser-sync').create();
 
 const js = require('./tasks/js').js(config.localFilesJs, config.localFilesJsOrder, config.localServerProjectPath);
 js.displayName = "js";
@@ -8,6 +10,22 @@ const hello = function (done) {
     done();
 };
 
+const css = function (done){
+    console.log("Css...");
+    done();
+};
+
+const watchFiles = () => {
+    browserSync.init({server: {baseDir: "./"}});
+
+    watch(["./css/*.css"], series(css));
+    //Meerdere keren watch aanroepen mag voor andere taken!
+    watch(["./css/*.css"]).on("change", browserSync.reload);
+};
+watchFiles.displayName = "watch";
+
 exports.default = hello;
 
 exports.js = js;
+
+exports.watch = watchFiles;
