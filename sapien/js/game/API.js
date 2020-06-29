ec1b8f6d562454f54c752355d30e50ad140ae5ee
Danny van Bokhorst <dannyvanbokhorst@live.nl>
2020-06-29 15:07:21 +0200

    game.API aangemaakt

---
+++ sapien/js/game/API.js
@@ -0,0 +1,14 @@
+Game.API = (function () {
+
+    let configMap = {
+    };
+
+    //Public method thanks to the return.
+    const init = function(){
+
+    };
+
+    return {
+        init: init
+    };
+})();
